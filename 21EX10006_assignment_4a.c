b8ced240c62d5bd06c60f0bde6ca73c104158c7d
Blackstocks <ankitkumarsoni656.iitkha@gmail.com>
2022-01-04 23:49:49 +0530

    assignment

---
+++ 21EX10006_assignment_4a.c
@@ -0,0 +1,25 @@
+//Name: Ankit kumar soni
+//Dept: geology and geophysics
+//roll no. 21EX10006
+//package: codeblock
+//operating system: windows 11
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i=1,j;
+    printf("plese enter the table you want: ");
+    scanf("%d",&n);
+    while (i<=10)
+    {
+        j=n*i;
+        printf("%d*%d=%d\n",n,i,j);
+        i++;
+    }
+    
+    return 0;
+    
+
+
+}
